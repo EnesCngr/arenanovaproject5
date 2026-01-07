111343550242878f76646f174e8be25586355fbb
Azra Celik <i576409@fontysict.nl>
2026-01-07 02:29:48 +0100

    chore: move to net8 + ui updates

--- arenanovaproject5-main/ArenaNovaProject5.Web/Program.cs
+++ arenanovaproject5-main/ArenaNovaProject5.Web/Program.cs
@@ -23,16 +23,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+// ✅ NET8 için şart: wwwroot (css/js) servis et
+app.UseStaticFiles();
 
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
