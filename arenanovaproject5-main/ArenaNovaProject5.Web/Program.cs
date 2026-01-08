05cdfb242eb2d20ac463f7ac21f1183e1e68d3ba
Enes Cingir <enes26cngr@gmail.com>
2026-01-08 06:56:04 +0100

    feat(auth): add route guard and auth session scaffolding

--- arenanovaproject5-main/ArenaNovaProject5.Web/Program.cs
+++ arenanovaproject5-main/ArenaNovaProject5.Web/Program.cs
@@ -16,6 +16,20 @@ builder.Services.AddScoped<FirebaseService>();
 
 // Register Firebase Auth Service with HttpClient
 builder.Services.AddHttpClient<FirebaseAuthService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthSessionService>();
+builder.Services.AddScoped<Microsoft.AspNetCore.Components.Authorization.AuthenticationStateProvider, FirebaseAuthStateProvider>();
+
+
+// Auth (Blazor Authorization)
+builder.Services.AddAuthorizationCore();
+
+// Session state
+builder.Services.AddScoped<AuthSessionService>();
+
+// Blazor auth state provider
+builder.Services.AddScoped<Microsoft.AspNetCore.Components.Authorization.AuthenticationStateProvider, FirebaseAuthStateProvider>();
+
 
 var app = builder.Build();
 
---
+++ arenanovaproject5-main/ArenaNovaProject5.Web/Services/FirebaseAuthStateProvider.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace ArenaNovaProject5.Web.Services;
+
+public class FirebaseAuthStateProvider : AuthenticationStateProvider
+{
+    private readonly AuthSessionService _session;
+
+    public FirebaseAuthStateProvider(AuthSessionService session)
+    {
+        _session = session;
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var user = _session.ToPrincipal();
+        return Task.FromResult(new AuthenticationState(user));
+    }
+
+    public void NotifyAuthChanged()
+    {
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+}
